a60a900edc3ff0bfe33b4f83d99900aa03dc280c
leonsuetfeld <mail@leonsuetfeld.com>
2018-08-30 14:53:34 +0200

    updated main file

--- logreg_analysis_3.R
+++ logreg_analysis_3.R
@@ -92,7 +92,7 @@ modelformula = choice_left ~
 modelprior = get_prior(modelformula, family=binomial, data=d1.hum.effect)
 priors <- c(set_prior("lkj(2)", class = "cor"),
             set_prior("normal(0,3)", class = "b"),
-            set_prior("cauchy(0,1)", class = "sd", group = "sn_idx"))   
+            set_prior("exponential(1)", class = "sd", group = "sn_idx")) # alternative: cauchy(0,1)
 # info on priors:
 # https://github.com/stan-dev/stan/wiki/Prior-Choice-Recommendations#prior-for-the-regression-coefficients-in-logistic-regression-non-sparse-case
 # http://jakewestfall.org/misc/SorensenEtAl.pdf
@@ -107,6 +107,10 @@ mres = brm(modelformula,
 # print results of fit
 summary(mres)
 
+# ###############################################
+# ### results and interpretation ################
+# ###############################################
+
 # results and interpretation:
 # Population-Level Effects: 
 #                                   Estimate Est.Error l-95% CI u-95% CI
@@ -118,8 +122,8 @@ summary(mres)
 
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # the above are main effects based on the mean of the abstraction levels
-# and the mean of the modality levels, right? and those values can be
-# interpreted as population means, correct?
+# and the mean of the modality levels, and they can be interpreted as
+# population means.
 # ----------------------------------------------------------------------
 
 # abstraction                           0.38      0.30    -0.20     0.95
@@ -131,10 +135,8 @@ summary(mres)
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # these are the effects of the abstraction level on the parameters.
 # in order to make predictions w.r.t. abstraction==1 (image), we would
-# essentially add the estimated value to the main effects, while making
-# predictions for abstraction==0 (text), we would subtract them, correct?
-# (at least I think that's how it should be, but I'm not sure if with the
-# current effect coding we would have to add/ subtract 0.5*parameter...?)
+# add 0.5 of the estimated value to the main effects, while making
+# predictions for abstraction==0 (text), we would subtract them.
 # ----------------------------------------------------------------------
 
 # modality                              0.16      0.34    -0.50     0.83
@@ -153,20 +155,63 @@ summary(mres)
 # elderly_diff:modality:abstraction     1.22      1.76    -2.23     4.76
 
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# these are interaction parameters and would be added if abstr==1 and
-# modal==1 at the same time, and subtracted wenn abstr==0 && modal==0,
-# correct?
+# these are interaction parameters and would be added (*0.5) if abstr==1 and
+# modal==1 at the same time, and subtracted (*0.5) wenn abstr==0 && modal==0.
 # ----------------------------------------------------------------------
 
 # questions / to do:
-# - in the model it's "visonset_left:abstraction" -- why not "visonset_left*abstraction"?
-# - is the coding on the correct scale to make predictions as suggested above?
 # - we should make predictions with this model and see if they fit the data on a descriptive level
 #   to make sure the model is making good predictions.
 # - to make a statement about whether or not something is "significant", you suggested bayes factor,
 #   but bayes factor is a tool for model comparison, right? so how can we use it to make statements
 #   about individual parameters? couldn't we just fit 4 models (one without A/M, one with A, one
 #   with M, one with both) and compare WAIC values?
+
+# results with exponential(1) prior on sd for the sn_idx group:
+# Population-Level Effects: 
+#                                   Estimate Est.Error l-95% CI u-95% CI Eff.Sample Rhat
+# Intercept                            -0.16      0.16    -0.47     0.14       4000 1.00
+# visonset_left                         1.30      0.51     0.31     2.36       3751 1.00
+# sex_diff                             -2.80      0.35    -3.52    -2.15       4000 1.00
+# young_diff                            8.34      1.03     6.51    10.59       2133 1.00
+# elderly_diff                         -6.72      0.88    -8.56    -5.13       2317 1.00
+
+# abstraction                           0.37      0.27    -0.13     0.90       4000 1.00
+# visonset_left:abstraction            -1.20      0.78    -2.77     0.31       4000 1.00
+# sex_diff:abstraction                 -0.28      0.64    -1.55     0.98       4000 1.00
+# young_diff:abstraction               -0.04      1.22    -2.43     2.29       4000 1.00
+# elderly_diff:abstraction              1.61      0.97    -0.27     3.53       4000 1.00
+
+# modality                              0.15      0.30    -0.45     0.75       4000 1.00
+# sex_diff:modality                     0.68      0.65    -0.57     1.95       4000 1.00
+# young_diff:modality                   1.10      1.50    -1.90     4.07       3513 1.00
+# elderly_diff:modality                 1.78      1.33    -0.88     4.42       3487 1.00
+
+# modality:abstraction                  0.23      0.54    -0.84     1.32       4000 1.00
+# sex_diff:modality:abstraction        -0.73      1.20    -3.15     1.61       4000 1.00
+# young_diff:modality:abstraction      -0.26      1.90    -3.96     3.54       4000 1.00
+# elderly_diff:modality:abstraction     1.21      1.66    -2.07     4.54       4000 1.00
+
+# Warning message:
+#   There were 17 divergent transitions after warmup. Increasing adapt_delta above 0.9 may help.
+# See http://mc-stan.org/misc/warnings.html#divergent-transitions-after-warmup 
+
+# ###############################################
+# ### posterior predictive check ################
+# ###############################################
+
+pp_check(mres, "data", nsamples = NULL, group = "sn_idx")
+
+# steps to manually sample from the distribution
+# 1) sample all population-level parameters from the posterior distribution
+# 2) sample 85 subjects (i.e., their individual parameter-offsets) from the distribution
+# 3) randomly split them up into 42 and 43 and assign them to the modalities
+# 4) get the distributions over the number of hum-hum trials, and sample two numbers
+#    of hum-hum trials for each participant (one for each abstraction level)
+# 5) take the exact trials from the data (obstacle left, obstacle right, starting lane) and encode
+#    them in the age_diff / sex_diff coding scheme
+# 6) for each trial, add the relevant parameters up and make a prediction
+# 7) repeat the process 1000 times and make a distribution of predictions?
 
 # ###############################################
 # ### plots #####################################
@@ -180,8 +225,8 @@ d1.hum.plot = d1.hum %>% mutate(sex_diff = factor(sex_diff, levels=c(-1,0,1), la
                                 visonset_left = factor(visonset_left, levels=c(-1,0,1), labels=c('visonsetLeft','none','visonsetRight')),
                                 elderly_diff = factor(elderly_diff, levels=c(-1,0,1), labels=c('oldLeftNotRight','noneOrBothOld','oldRightNotLeft')),
                                 young_diff = factor(young_diff, levels=c(-1,0,1), labels=c('youngLeftNotRight','noneOrBothYoung','youngRightNotLeft')),
-                                abstraction = factor(abstraction, levels=c(0,1), labels=c('text',"image")),
-                                modality = factor(modality, levels=c(0,1), labels=c('desktop',"vr")))
+                                abstraction = factor(abstraction, levels=c(0,1), labels=c('text','image')),
+                                modality = factor(modality, levels=c(0,1), labels=c('desktop','vr')))
 
 # plots: what are these exactly? data based / descriptive plots? where do the error bars come from?
 # error message: "No summary function supplied, defaulting to `mean_se()" -- ?
